8a40ed5dd0dcd5ef1eaffd2abb2b2a3ac2236ba9
roseleaf <pteroline@gmail.com>
2012-07-27 17:22:09 -0700

    moving

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppAcademySample::Application.config.secret_token = 'f69e3d18a2a530fce2cf2a6de9983fc5347b322d78cd2b6a40ca87395a66a8687cadb2aefb4f1044ee9d1d16291e766376a6c0d844252d00b05701bc9400dbac'
